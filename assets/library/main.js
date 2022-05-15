9abaeef3a3b43fb201ef7667b49b6ffc2e63ce9c
Leonardo <leonardo.ciaccio@gmail.com>
2022-05-15 18:24:56 +0200

    1.017
    Design and progress

--- assets/library/main.js
+++ assets/library/main.js
@@ -1,6 +1,31 @@
 (function() {
 
-    $('#shop-container .menu .item')
-        .tab();
+    $('#shop-container .menu .item').tab()
+        /*.tab({
+
+                evaluateScripts: 'once',
+                context: 'parent',
+                auto: true,
+                path: '/assets/frames/'
+
+            })*/
+    ;
+
+    $('.open-main-menu').click(function() {
+
+        $('#main-menu')
+            .modal({
+                closeIcon: true
+            })
+            .modal('show');
+
+    });
+
+    $('.faq.accordion')
+        .accordion({
+            selector: {
+                trigger: '.title'
+            }
+        });
 
 })();
